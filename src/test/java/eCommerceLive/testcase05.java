c23fdb5c8f7f9b6448c93a3102146ff109910d5a
ahmed <ahmeddola20@>
2024-08-09 19:03:57 +0300

    8-8-2024

--- src/test/java/eCommerceLive/testcase05.java
+++ src/test/java/eCommerceLive/testcase05.java
@@ -13,7 +13,7 @@ import org.testng.annotations.Test;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public  class testcase04 {
+public  class testcase05 {
     private WebDriver driver;
     private String baseUrl;
 
